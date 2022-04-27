b5397a8d91a5b0a11687dd7425b56c7aad096446
Dmitriy <0d13rn@gmail.com>
2022-04-27 20:03:23 +0300

    changed assert function at test_items.py

--- 3_part_main_project/test_items.py
+++ 3_part_main_project/test_items.py
@@ -1,11 +1,16 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 
-def test_add_busket_button(browser):
-   
+def test_find_add_item_to_busket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    browser.implicitly_wait(5)
+    browser.implicitly_wait(3)
 
-    browser.find_element_by_css_selector("form#add_to_basket_form button.btn")
+    try:
+        browser.find_element_by_css_selector("form#add_to_basket_form button.btn")
+
+    except NoSuchElementException:
+        assert False, "Button wasn't found"
+    
